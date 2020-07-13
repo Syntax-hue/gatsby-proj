c87887bff2b6bd1d133f512a14b6d038e99ccb54
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2020-07-13 23:10:51 +0800

    add menu

--- src/pages/.from.js
+++ src/pages/.from.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import '../styles/form.css'
 
 export default function Form() {
+  const selectElem = useRef();
+  const selectWrapElem = useRef();
+  const handleOpenMenu = () => {
+    selectWrapElem.current.classList.toggle('selectWrap_open');
+    const changeClass = (e) => {
+      selectWrapElem.current.classList.toggle('selectWrap_open');
+      selectElem.current.blur();
+      window.removeEventListener('click', changeClass, false)
+    }
+    window.addEventListener('click', changeClass, false)
+  }
+
   return (
     <form className="submit_from">
       <h3>
@@ -14,8 +26,10 @@ export default function Form() {
       <div>
         <input placeholder="Telefon de contact"/>
       </div>
-      <div className="selectWrap" tabIndex="0">
-        <select defaultValue="Tip pașaport">
+      <div ref={selectWrapElem} className="selectWrap" tabIndex="0">
+        <select onFocus={handleOpenMenu}
+                ref={selectElem} 
+                defaultValue="Tip pașaport">
           <option>1</option>
           <option>2</option>
           <option style={{display: 'none'}} value="Tip pașaport">Tip pașaport</option>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React, { useRef } from "react"
 import '../styles/index.scss'
-import {useStaticQuery, graphql} from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 import Form from "./.from"
 
 
@@ -21,93 +21,118 @@ const IndexPage = () => {
     `
   )
   const allImages = {};
-  data.allFile.edges.map(({node}) => {
+  data.allFile.edges.map(({ node }) => {
     allImages[node.name] = node.publicURL
   })
 
+  const blockMenu = useRef();
+  const btnMenu = useRef();
+  const handleOpenMenu = (e) => {
+    btnMenu.current.classList.toggle('burger-open')
+    blockMenu.current.classList.toggle('linksBlockMobileMenu_open')
+  }
+
   return (
-  <div className="bigContiner">
-    <header>
-      <div className="headerLineTop">
-        <div className="continer d-f f-between">
-          <div className="d-f connectBlock">
-            <div className="phoneBlock">
-              <img src={allImages['Path_140']} alt=""/>
-              <a href="tel:+37322211808">
-                +373 22 211 808
-              </a>
-            </div>
-            <div className="mailBlock">
-              <img src={allImages['Path_141']} alt=""/>
-              <a href="mailto:hr.legis@gmail.com">
-                hr.legis@gmail.com
-              </a>
-            </div>
+    <div className="bigContiner">
+      <header>
+        <div ref={blockMenu} className="d-f f-al-center continer linksBlock linksBlockMobileMenu">
+          <div className="d-f f-between linksBlock__links">
+            <a href="#">
+              Locuri vacante
+            </a>
+            <a href="#">
+              Despre noi
+            </a>
+            <a href="#">
+              Contacte
+            </a>
           </div>
-          <div className="d-f sotialBlock">
-            <a href="#" className="sotialBlock__item">
-              <img src={allImages['q1']} alt=""/>
-            </a>
-            <a href="#" className="sotialBlock__item">
-              <img src={allImages['q2']} alt=""/>
-            </a>
-            <a href="#" className="sotialBlock__item">
-              <img src={allImages['q3']} alt=""/>
-            </a>
-            <a href="#" className="sotialBlock__item">
-              <img src={allImages['q4']} alt=""/>
-            </a>
+          <div className="d-f linksBlock__buttons">
+            <button className="orange_button">
+              Licența
+          </button>
+            <button className="orange_button">
+              RETURNARE IMPOZITE
+          </button>
           </div>
         </div>
-      </div>
-      <div className="headerLineBottom">
-        <div className="d-f f-between continer">
-          <div>
-            <img className="logoHeader" src={allImages['Legis_R']}/>
-          </div>
-          <div className="d-f f-al-center linksBlock">
-            <div className="d-f f-between linksBlock__links">
-              <a href="#">
-                Locuri vacante
+        <div className="headerLineTop">
+          <div className="continer d-f f-between">
+            <div className="d-f connectBlock">
+              <div className="phoneBlock">
+                <img src={allImages['Path_140']} alt="" />
+                <a href="tel:+37322211808">
+                  +373 22 211 808
+                </a>
+              </div>
+              <div className="mailBlock">
+                <img src={allImages['Path_141']} alt="" />
+                <a href="mailto:hr.legis@gmail.com">
+                  hr.legis@gmail.com
               </a>
-              <a href="#">
-                Despre noi
-              </a>
-              <a href="#">
-                Contacte
-              </a>
+              </div>
             </div>
-            <div className="d-f linksBlock__buttons">
-              <button className="orange_button">
-                Licența
-              </button>
-              <button className="orange_button">
-                RETURNARE IMPOZITE
-              </button>
-              <button className="burger">
-                <span>
-                  
-                </span>
-              </button>
+            <div className="d-f sotialBlock">
+              <a href="#" className="sotialBlock__item">
+                <img src={allImages['q1']} alt="" />
+              </a>
+              <a href="#" className="sotialBlock__item">
+                <img src={allImages['q2']} alt="" />
+              </a>
+              <a href="#" className="sotialBlock__item">
+                <img src={allImages['q3']} alt="" />
+              </a>
+              <a href="#" className="sotialBlock__item">
+                <img src={allImages['q4']} alt="" />
+              </a>
             </div>
           </div>
         </div>
-      </div>
-    </header>
-    <section className="formBlock">
-      <div className="continer">
-        <h1>
-          Aveți nevoie de un loc de&nbsp;muncă&nbsp;legal&nbsp;în&nbsp;Europa?
-        </h1>
-        <h2>
-        Plecările legale în Europa sînt în baza&nbsp;Pașaportului&nbsp;European
-        </h2>
-        <Form />
-      </div>
-      <img className="fonImg" src={allImages['fon']}/>
-    </section>
-  </div>
+        <div className="headerLineBottom">
+          <div className="d-f f-between continer">
+            <img className="logoHeader" src={allImages['Legis_R']} />
+            <div className="d-f f-al-center linksBlock">
+              <div className="d-f f-between linksBlock__links">
+                <a href="#">
+                  Locuri vacante
+                </a>
+                <a href="#">
+                  Despre noi
+                </a>
+                <a href="#">
+                  Contacte
+                </a>
+              </div>
+              <div className="d-f linksBlock__buttons">
+                <button className="orange_button">
+                  Licența
+                </button>
+                <button className="orange_button">
+                  RETURNARE IMPOZITE
+                </button>
+                <button ref={btnMenu} onClick={handleOpenMenu} className="burger">
+                  <span>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section className="formBlock">
+        <div className="continer">
+          <h1>
+            Aveți nevoie de un loc de&nbsp;muncă&nbsp;legal&nbsp;în&nbsp;Europa?
+          </h1>
+          <h2>
+            Plecările legale în Europa sînt în baza&nbsp;Pașaportului&nbsp;European
+          </h2>
+          <Form />
+        </div>
+        <img className="fonImg" src={allImages['fon']} />
+      </section>
+    </div>
   )
-} 
+}
 
 export default IndexPage
